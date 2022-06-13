ee49b11c07f2169267935b3aab90a00381afb363
Mark Schmitz <mark.schmitz@mailbox.org>
2022-06-13 14:44:38 +0100

    upd build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-#!/bin/bash -eux
+#!/usr/bin/env bash 
+
+set -eux
 
 # Simple build script for macOS
 
@@ -8,12 +10,12 @@ _src_dir="$_root_dir/build/src"
 _main_repo="$_root_dir/ungoogled-chromium"
 
 # For packaging
-_chromium_version=$(cat $_root_dir/ungoogled-chromium/chromium_version.txt)
-_ungoogled_revision=$(cat $_root_dir/ungoogled-chromium/revision.txt)
-_package_revision=$(cat $_root_dir/revision.txt)
+_chromium_version=$(cat "$_root_dir"/ungoogled-chromium/chromium_version.txt)
+_ungoogled_revision=$(cat "$_root_dir"/ungoogled-chromium/revision.txt)
+_package_revision=$(cat "$_root_dir"/revision.txt)
 
 # Add local clang and build tools to PATH
-export PATH="$PATH:$_src_dir/third_party/llvm-build/Release+Asserts/bin"
+# export PATH="$PATH:$_src_dir/third_party/llvm-build/Release+Asserts/bin"
 
 rm -rf "$_src_dir/out" || true
 mkdir -p "$_src_dir/out/Default"
@@ -24,13 +26,9 @@ mkdir -p "$_download_cache"
 "$_main_repo/utils/prune_binaries.py" "$_src_dir" "$_main_repo/pruning.list"
 "$_main_repo/utils/patches.py" apply "$_src_dir" "$_main_repo/patches" "$_root_dir/patches"
 "$_main_repo/utils/domain_substitution.py" apply -r "$_main_repo/domain_regex.list" -f "$_main_repo/domain_substitution.list" -c "$_root_dir/build/domsubcache.tar.gz" "$_src_dir"
-cp "$_main_repo/flags.gn" "$_src_dir/out/Default/args.gn"
-cat "$_root_dir/flags.macos.gn" >> "$_src_dir/out/Default/args.gn"
+cat "$_main_repo/flags.gn" "$_root_dir/flags.macos.gn" > "$_src_dir/out/Default/args.gn"
 
 cd "$_src_dir"
-
-# dsymutilpath fix
-ln -sv "$_src_dir/third_party/llvm-build/Release+Asserts/bin" "$_src_dir/tools/clang/dsymutil/bin"
 
 ./tools/gn/bootstrap/bootstrap.py -o out/Default/gn --skip-generate-buildfiles
 ./out/Default/gn gen out/Default --fail-on-unused-args
